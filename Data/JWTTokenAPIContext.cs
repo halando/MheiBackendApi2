f526f24f0acfba5ec83de5ebfb46c5d0ce697227
Andó Attila <andoattila698@gmail.com>
2024-04-18 22:07:59 +0200

    Api

---
+++ Data/JWTTokenAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JWTTokenAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace JWTTokenAPI.Data
+{
+    public class JWTTokenAPIContext : IdentityDbContext<ApplicationUser>
+    {
+        public JWTTokenAPIContext (DbContextOptions<JWTTokenAPIContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JWTTokenAPI.Models.Event>? Event { get; set; }
+
+        //public DbSet<JWTTokenAPI.Models.Company> Company { get; set; } = default!;
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,23 @@
+﻿namespace JWTTokenAPI.Models
+{
+    public class Event
+    {
+
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? ImagePath = null;
+
+
+        public string? Description { get; set; }
+
+        public string? Link { get; set; }
+
+
+
+
+
+    }
+}
